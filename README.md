04cc2cb787e31c5de926279e58f02b7e9de15037
Carlos Felipe Gomes <carlos.gomes@bigdatacorp.com.br>
2025-10-28 01:33:21 -0300

    build: first deploy

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # homelab-gitops
+
+With this Lab you will be able to access any service installed on this track, from any device in the same network(subnet)!
+
+## Order2reproduce
+
+### 00. Prepare environment
+
+- Ubuntu 24.04 amd64
+- Execute `install-base.sh`
+    - Install docker, kubectl, helm, helmfile, network basics
+    - Install Minikube
+    - Enable MetalLB
+    - Change IpAddress to MetalLB (change to your local network)
+
+### 01. Install Basic HelmCharts to automate your environment.
+
+- Execute `start-gitops.sh` to install the following charts.
+    - external-dns
+    - cert-manager
+    - nginx
+    - argo
+
+### 02. Now everything is through ArgoCD.
+
+- Waves
+- App-to-Apps
+- Sync
---
+++ install-base.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+########### BASIC CONFIG TO AUTOMATE
+# Install Basic for Ansible Automation
+sudo apt update
+sudo apt install -y python3 python3-pip
+pip install ansible-core --break-system-packages
+
+########### BASIC CONFIG TO DOCKER
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+########### INSTALL KUBECTL
+sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.34/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
+# Install Package
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+########### INSTALL HELM
+sudo apt-get install curl gpg apt-transport-https --yes
+curl -fsSL https://packages.buildkite.com/helm-linux/helm-debian/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://packages.buildkite.com/helm-linux/helm-debian/any/ any main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+########### INSTALL HELMFILE
+wget https://github.com/helmfile/helmfile/releases/download/v1.1.8/helmfile_1.1.8_linux_amd64.tar.gz
+tar xvzf helmfile_1.1.8_linux_amd64.tar.gz -C /tmp
+sudo mv /tmp/helmfile /usr/local/bin/helmfile
+
+########### BASIC CONFIG TO NETWORK
+sudo apt update
+sudo apt install -y net-tools conntrack iproute2 curl wget socat ebtables ethtool
+#Install CNI PLUGIN
+CNI_PLUGIN_VERSION="v1.8.0"
+CNI_PLUGIN_TAR="cni-plugins-linux-amd64-$CNI_PLUGIN_VERSION.tgz" # change arch if not on amd64
+CNI_PLUGIN_INSTALL_DIR="/opt/cni/bin"
+curl -LO "https://github.com/containernetworking/plugins/releases/download/$CNI_PLUGIN_VERSION/$CNI_PLUGIN_TAR"
+sudo mkdir -p "$CNI_PLUGIN_INSTALL_DIR"
+sudo tar -xf "$CNI_PLUGIN_TAR" -C "$CNI_PLUGIN_INSTALL_DIR"
+rm "$CNI_PLUGIN_TAR"
+
+########### BASIC CONFIG TO MINIKUBE
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+# START BASIC CONFIG TO MINIKUBE
+minikube start --driver=none
+minikube addons enable metallb
+kubectl -n metallb-system delete configmap config
+kubectl apply -f pre-helm/metallb.yaml
